b6caf41da10d46367a489395726bd0e49ae8059a
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2021-09-07 21:25:28 -0300

    Set constructor for Sale and set the seatMAp array

--- CineApp/src/cinema/Sale.java
+++ CineApp/src/cinema/Sale.java
@@ -2,8 +2,17 @@ package cinema;
 
 public class Sale {
 	private int saleId;
-	private Receipt receipt = new Receipt();
-	private Employee employee = new Employee();
+	private Receipt receipt;
+	private Employee employee;
+	
+	public Sale(Employee employee, Receipt receipt) {
+		final int max = 100000;
+		final int min = 1;
+		this.saleId = (int)Math.floor(Math.random()*(max-min+1)+min);
+		this.receipt = receipt;
+		this.employee = employee;
+	}
+	
 	public int getSaleId() {
 		return saleId;
 	}
